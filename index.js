0e0e1c5c975ca9a1c7f443bad4df66949eb7bfc3
cchuuu <chuyuanz@nycstudents.net>
2024-10-07 12:20:22 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const students = [
+  { name: "Alice", age: 20, grade: 85 },
+  { name: "Bob", age: 22, grade: 90 },
+  { name: "Charlie", age: 21, grade: 78 },
+  { name: "Diana", age: 23, grade: 95 },
+];
+
+students.forEach((student) => console.log(student.name, student.grade));
+
+const topStudents = students.filter((student) => student.grade > 80);
+console.log(topStudents);
+
+const youngStudents = students.filter((student) => student.age < 21);
+
+const names = students.forEach((student) => console.log(student.name));
+
+const topStudentsNames = names.filter((student) => student.grade > 80);
+
+console.log(topStudentsNames);
